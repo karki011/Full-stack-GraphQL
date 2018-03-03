112ca7a6ed0c55cd8d8d8218dffce0df2de8d8d5
subash karki <karki011@gmail.com>
2018-03-03 11:28:23 -0600

    set up Apollo server

--- imports/startup/client/index.js
+++ imports/startup/client/index.js
@@ -4,7 +4,7 @@ import { render } from 'react-dom';
 
 //set up basic react component
 
-const App = () =>  <h1> Hello</h1>;
+import App from "../../ui/app";
 
 Meteor.startup(() => {
     render(<App />, document.getElementById("app"));
